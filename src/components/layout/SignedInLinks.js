585ee010d6c8a2b708c976d042802ee9ccf6dcd2
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-03-09 13:28:25 -0800

    add hotellist links to navbar

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import { connect } from 'react-redux';
 import { signOut } from '../../store/actions/authActions';
+import HotelList from '../hotels/HotelList';
 
 const SignedInLinks = (props) => {
   return(
     <ul className="right">
+      <li><NavLink to="/hotelList">Browse Our Hotels</NavLink></li>
       <li><NavLink to="/create">Leave a Review</NavLink></li>
       <li><a onClick={props.signOut}>Log Out</a></li>
       <li><NavLink to="/" className = "btn btn-floating red darken-3">
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import HotelList from '../hotels/HotelList';
 
 const SignedOutLinks = () => {
   return(
     <ul className="right">
+      <li><NavLink to="/hotelList">Browse Our Hotels</NavLink></li>
       <li><NavLink to="/signup">Signup</NavLink></li>
       <li><NavLink to="/signin">Login</NavLink></li>
     </ul>
